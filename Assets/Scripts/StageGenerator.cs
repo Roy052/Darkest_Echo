c237e1e75fc6a334692bb81969cacf426e1594fa
Roy052 <32808714+Roy052@users.noreply.github.com>
2023-07-21 00:17:53 +0900

    [수정] Stage Generator 작업중
    저장 / 불러오기 몇 가지만 됨

--- Assets/Scripts/StageGenerator.cs
+++ Assets/Scripts/StageGenerator.cs
@@ -7,10 +7,14 @@ using System.IO;
 public class StageData
 {
     public SerializableTransform player;
+    public SerializableTransform endZone;
 
     public List<SerializableTransform> walls = new List<SerializableTransform>();
-    public List<SerializableObject> enemys = new List<SerializableObject>();
-    public List<SerializableObject> objects = new List<SerializableObject>();
+
+    public List<int> enemyTypes = new List<int>();
+    public List<int> objectTypes = new List<int>();
+    public List<SerializableTransform> enemys = new List<SerializableTransform>();
+    public List<SerializableTransform> objects = new List<SerializableTransform>();
 }
 
 [System.Serializable]
@@ -61,18 +65,25 @@ public class StageGenerator : MonoBehaviour
     [SerializeField] private StageData stageData;
 
     public Transform playerParent;
+    public Transform endZoneParent;
     public Transform wallsParent;
     public Transform enemysParent;
     public Transform objectsParent;
 
+    public GameObject wallPrefab;
+    public GameObject[] enemyPrefab;
+    public GameObject[] objectPrefab;
+
     public void SaveStageData(int stageNum)
     {
         stageData = new StageData();
+
+        //Player
         for (int i = 0; i < playerParent.childCount; i++)
         {
-            if (playerParent.GetChild(0).gameObject.activeSelf)
+            if (playerParent.GetChild(i).gameObject.activeSelf)
             {
-                stageData.player = new SerializableTransform(playerParent.GetChild(0).transform);
+                stageData.player = new SerializableTransform(playerParent.GetChild(i).transform);
                 break;
             }
         }
@@ -83,22 +94,49 @@ public class StageGenerator : MonoBehaviour
             return;
         }
 
-        for(int i = 0; i < wallsParent.childCount; i++)
+        //EndZone
+        for (int i = 0; i < endZoneParent.childCount; i++)
         {
-            stageData.walls.Add(new SerializableTransform(wallsParent.GetChild(i).transform));
+            if (endZoneParent.GetChild(i).gameObject.activeSelf)
+            {
+                stageData.endZone = new SerializableTransform(endZoneParent.GetChild(i).transform);
+                break;
+            }
         }
 
+        if (stageData.endZone == null)
+        {
+            Debug.LogError("EndZone No Exist Error");
+            return;
+        }
+
+        //Wall
+        for (int i = 0; i < wallsParent.childCount; i++)
+        {
+            if (wallsParent.GetChild(i).gameObject.activeSelf)
+                stageData.walls.Add(new SerializableTransform(wallsParent.GetChild(i).transform));
+        }
+
+        //Enemy
         for(int i = 0; i < enemysParent.childCount; i++)
         {
-            stageData.enemys.Add(new SerializableObject() 
-            { type = 0, transform = new SerializableTransform(enemysParent.GetChild(i).transform) });
+            if (enemysParent.GetChild(i).gameObject.activeSelf)
+            {
+                stageData.enemyTypes[i] = 0;
+                stageData.enemys.Add(new SerializableTransform(enemysParent.GetChild(i).transform));
+            }
+                
         }
 
-        for (int i = 0; i < enemysParent.childCount; i++)
+        //Object
+        for (int i = 0; i < objectsParent.childCount; i++)
         {
-            stageData.enemys.Add(new SerializableObject()
-            { type = (int) enemysParent.GetChild(i).GetComponent<StageObject>().type, 
-                transform = new SerializableTransform(enemysParent.GetChild(i).transform) });
+            if (objectsParent.GetChild(i).gameObject.activeSelf)
+            {
+                stageData.objectTypes.Add((int) objectsParent.GetChild(i).gameObject.GetComponent<StageObject>().type);
+                stageData.objects.Add(new SerializableTransform(objectsParent.GetChild(i).transform));
+            }
+                
         }
 
         string json = JsonUtility.ToJson(stageData);
@@ -111,8 +149,107 @@ public class StageGenerator : MonoBehaviour
     {
         string data = File.ReadAllText(path + "/" + fileName + stageNum);
         Debug.Log(data);
-        StageData file = JsonUtility.FromJson<StageData>(data);
-        Debug.Log(file.player.position);
+        StageData stageData = JsonUtility.FromJson<StageData>(data);
+
+        if (stageData.player == null || stageData.endZone == null)
+        {
+            Debug.LogError("Stage Data No Exist Error");
+            return;
+        }
+
+        //Player
+        Transform trPlayer = playerParent.GetChild(0).gameObject.transform;
+        trPlayer.position = stageData.player.position;
+        trPlayer.eulerAngles = stageData.player.rotation;
+        trPlayer.localScale = stageData.player.scale;
+        trPlayer.SetAsFirstSibling();
+
+        //EndZone
+        Transform trEndZone = endZoneParent.GetChild(0).gameObject.transform;
+        trEndZone.position = stageData.endZone.position;
+        trEndZone.eulerAngles = stageData.endZone.rotation;
+        trEndZone.localScale = stageData.endZone.scale;
+        trEndZone.SetAsFirstSibling();
+
+        //Wall
+        int count = wallsParent.childCount;
+        for (int i = stageData.walls.Count - 1; i >= 0; i--)
+        {
+            if(i >= count - 1)
+            {
+                GameObject objWall = Instantiate(wallPrefab, wallsParent);
+                objWall.SetActive(true);
+
+                Transform trWall = objWall.transform;
+                trWall.position = stageData.walls[i].position;
+                trWall.eulerAngles = stageData.walls[i].rotation;
+                trWall.localScale = stageData.walls[i].scale;
+                trWall.SetAsFirstSibling();
+            }
+            else
+            {
+                Transform trWall = wallsParent.GetChild(i);
+                trWall.position = stageData.walls[i].position;
+                trWall.eulerAngles = stageData.walls[i].rotation;
+                trWall.localScale = stageData.walls[i].scale;
+                trWall.SetAsFirstSibling();
+            }
+        }
+
+        //Enemy
+        count = enemysParent.childCount;
+        while(enemysParent.childCount > 1)
+        {
+
+        }
+
+        for (int i = stageData.enemys.Count - 1; i >= 0; i--)
+        {
+            if (i >= count - 1)
+            {
+                GameObject objEnemy = Instantiate(enemyPrefab[stageData.enemyTypes[i]], enemysParent);
+                objEnemy.SetActive(true);
+
+                Transform trEnemy = objEnemy.transform;
+                trEnemy.position = stageData.enemys[i].position;
+                trEnemy.eulerAngles = stageData.enemys[i].rotation;
+                trEnemy.localScale = stageData.enemys[i].scale;
+                trEnemy.SetAsFirstSibling();
+            }
+            else
+            {
+                Transform trEnemy = enemysParent.GetChild(i);
+                trEnemy.position = stageData.enemys[i].position;
+                trEnemy.eulerAngles = stageData.enemys[i].rotation;
+                trEnemy.localScale = stageData.enemys[i].scale;
+                trEnemy.SetAsFirstSibling();
+            }
+        }
+
+        //Object
+        count = objectsParent.childCount;
+        for (int i = stageData.objects.Count - 1; i >= 0; i--)
+        {
+            if (i >= count - 1)
+            {
+                GameObject objObject = Instantiate(objectPrefab[stageData.objectTypes[i]], objectsParent);
+                objObject.SetActive(true);
+
+                Transform trObject = objObject.transform;
+                trObject.position = stageData.objects[i].position;
+                trObject.eulerAngles = stageData.enemys[i].rotation;
+                trObject.localScale = stageData.enemys[i].scale;
+                trObject.SetAsFirstSibling();
+            }
+            else
+            {
+                Transform trObject = objectsParent.GetChild(i);
+                trObject.position = stageData.objects[i].position;
+                trObject.eulerAngles = stageData.enemys[i].rotation;
+                trObject.localScale = stageData.enemys[i].scale;
+                trObject.SetAsFirstSibling();
+            }
+        }
     }
 
     public static bool isImmortal;
